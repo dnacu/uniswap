ddde66900c9bc60925f5f9914642187171a0e8e5
한규현 <winteri1276@gmail.com>
2023-03-04 17:22:48 +0900

    feat: Container 컴포넌트를 구성한다

---
+++ src/components/common/Container/Container.tsx
@@ -0,0 +1,24 @@
+import { BREAKPOINTS } from '@styles/breakpoints'
+import { PaddingProps, paddingStyles } from '@styles/core/padding'
+import styled, { css } from 'styled-components'
+
+type ContainerProps = PaddingProps & {
+  size: 'sm' | 'md'
+}
+
+export const Container = styled.div<ContainerProps>`
+  ${({ size, ...styleProps }) => css`
+    ${paddingStyles(styleProps)}
+
+    ${size === 'sm' && `max-width: 480px;`}
+    ${size === 'md' && `max-width: 640px;`}
+  `}
+
+  width: 100%;
+  margin: 0 auto;
+  overflow-x: hidden;
+
+  @media ${BREAKPOINTS.MOBILE} {
+    width: calc(100% - 40px);
+  }
+`
