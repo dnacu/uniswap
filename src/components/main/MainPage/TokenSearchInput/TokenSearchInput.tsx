933adcf615acfd5dfe9aa2bd914f735802246d6b
한규현 <winteri1276@gmail.com>
2023-03-05 01:30:47 +0900

    feat: 토큰 검색창을 구성한다

--- src/components/main/MainPage/TokenSearchInput/TokenSearchInput.tsx
+++ src/components/main/MainPage/TokenSearchInput/TokenSearchInput.tsx
@@ -1,7 +1,38 @@
+import { SearchIcon } from '@components/common/svgs/SearchIcon'
+import { VStack } from '@components/common/VStack'
 import { FC } from 'react'
+import styled from 'styled-components'
 
-type TokenSearchInputProps = {}
-
-export const TokenSearchInput: FC<TokenSearchInputProps> = () => {
-  return <div>TokenSearchInput</div>
+type TokenSearchInputProps = {
+  className?: string
 }
+
+export const TokenSearchInput: FC<TokenSearchInputProps> = ({ className }) => {
+  return (
+    <StyledVStack>
+      <StyledSearchIcon />
+      <StyledInput className={className} type="text" placeholder="토큰 이름을 입력하세요" />
+    </StyledVStack>
+  )
+}
+
+const StyledVStack = styled(VStack)`
+  position: relative;
+`
+
+const StyledSearchIcon = styled(SearchIcon)`
+  position: absolute;
+  left: 12px;
+  top: 10px;
+`
+
+const StyledInput = styled.input`
+  padding: 16px 16px 16px 40px;
+  height: 40px;
+  border: 1px solid rgb(27, 34, 54);
+  border-radius: 12px;
+  appearance: none;
+  font-size: 16px;
+  color: white;
+  background-color: rgb(19, 26, 42);
+`
